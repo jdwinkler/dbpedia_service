f5028f0e4305b6de2d65b4da15b717639bbc8efb
James Winkler <James Winkler>
2018-02-16 17:25:59 -0600

    small tweaks

--- database_query_handler.py
+++ database_query_handler.py
@@ -1,9 +1,14 @@
 import psycopg2
 import psycopg2.extras
-import os
-from collections import defaultdict
+
 
 class DBHandler:
+
+    """
+    
+    Handles I/O concerning the database to hide its implementation from client services.
+    
+    """
 
     def __init__(self):
 
@@ -13,7 +18,9 @@ class DBHandler:
         user_name = 'dbpedia_app'
         password = 'dummy_password'
 
-        connection = psycopg2.connect("dbname='postgres' user='%s' host='localhost' password='%s'"
+        # check to see if the db exists locally, create it if necessary
+        connection = psycopg2.connect("dbname='postgres' user='%s' "
+                                      "host='localhost' password='%s'"
                                       % ('postgres', 'password'))
         connection.autocommit = True
         cursor = connection.cursor()
@@ -22,7 +29,7 @@ class DBHandler:
         not_exists_row = cursor.fetchone()
         not_exists = not_exists_row[0]
         if not_exists:
-            cursor.execute('CREATE USER %s PASSWORD %s' % (user_name, password))
+            cursor.execute("CREATE USER %s PASSWORD '%s'" % (user_name, password))
             cursor.execute('CREATE DATABASE dbpedia OWNER %s' % (user_name,))
 
         connection.close()
@@ -30,12 +37,68 @@ class DBHandler:
         connection = psycopg2.connect("dbname='dbpedia' user='%s' host='localhost' password='%s'"
                                       % (user_name, password))
 
-        connection.set_session(readonly=True)
-        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
         self.connection = connection
-        self.cursor = cursor
 
     def __del__(self):
 
         self.connection.close()
+
+    def commit(self):
+
+        self.connection.commit()
+
+    def build_table_schema(self, schema_name, schema_file_path):
+
+        """
+        
+        Loads the dbpedia schema used for supporting downstream analysis. If the schema already exists, it is
+        dropped (deleted) and recreated.
+        
+        :param schema_name: 
+        :param schema_file_path: 
+        :return: 
+        """
+
+        # do not call with user input given the manual query construction here
+
+        with self.connection.cursor() as cursor:
+
+            cursor.execute('DROP SCHEMA IF EXISTS %s CASCADE' % schema_name)
+            schema_file = open(schema_file_path, 'rU').read()
+            cursor.execute(schema_file)
+
+    def insert_spo_tuple(self, spo_tuple):
+
+        """
+        
+        Handles the insertion of spo tuples into the db. Workflow:
+        
+        Attempt to find the subject table entry corresponding to your subject. If found, use that ID for
+        inserting your po values. Otherwise, insert your subject into the subject table and use that ID
+        instead. The resulting id, predicate, object tuple is then inserted into the predicate_object table.
+        
+        :param spo_tuple: 
+        :return: 
+        """
+
+        (subject, predicate, db_object) = spo_tuple
+
+        with self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+
+            cursor.execute('select subject_id from dbpedia.subjects '
+                           'where name = %s', (subject,))
+
+            results = cursor.fetchone()
+
+            if results is None or len(results) == 0:
+
+                cursor.execute('INSERT INTO dbpedia.subjects (name) VALUES (%s) '
+                               'returning subject_id', (subject,))
+                results = cursor.fetchone()
+
+            id = results['subject_id']
+
+            # now we have the correct id in either case, insert the values into the db
+
+            cursor.execute('INSERT INTO dbpedia.predicate_object (subject_id, predicate, object) '
+                           'VALUES (%s, %s, %s)', (id, predicate, db_object))
--- dbpedia_file_parser.py
+++ dbpedia_file_parser.py
@@ -33,6 +33,14 @@ def build_tql_file_generator(filepath):
 
 def plain_subject_predicate(line):
 
+    """
+    
+    Converts spo format into something approximately human readable.
+    
+    :param line: 
+    :return: 
+    """
+
     if '22-rdf-syntax-ns#' in line:
         return line.split('/')[-1].split('#')[-1]
     else:
@@ -41,16 +49,30 @@ def plain_subject_predicate(line):
 
 if __name__ == '__main__':
 
+    db_handler = DBHandler()
+
+    db_handler.build_table_schema('dbpedia', os.path.join(os.getcwd(), 'sql', 'dbpedia_schema.sql'))
+
     generator = build_tql_file_generator(os.path.join(os.getcwd(), 'dbpedia', 'persondata_en.tql'))
     g = rdflib.graph.ConjunctiveGraph()
+    counter = 0
 
     for line in generator:
 
+        # this is much slower than having a single graph read once
+        # however, this requires much less memory given that
+        # rdflib does not support reading from streams
         g = rdflib.graph.ConjunctiveGraph()
         g.parse(data=line, format='nquads')
 
-        for (subject, predicate, object) in g:
+        for (subject, predicate, db_object) in g:
 
             subject = plain_subject_predicate(subject)
             predicate = plain_subject_predicate(predicate)
-            object = plain_subject_predicate(object)
+            db_object = plain_subject_predicate(db_object)
+
+            db_handler.insert_spo_tuple((subject, predicate, db_object))
+
+        counter += 1
+
+    db_handler.commit()
